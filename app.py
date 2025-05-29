84b2559adbf31558fdc4f7768df7c906112f9f7b
DedeVictor <victorweta2006@gmail.com>
2025-05-29 06:43:04 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_scss import Scss
+from datetime import datetime
+
+#Flask App
+app = Flask(__name__)
+Scss(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFCATION"] = False
+db = SQLAlchemy(app)
+
+
+#Data class
+class MyTask(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   content = db.Column(db.String(100), nullable = False)
+   complete = db.Column(db.Integer, default = 0)
+   created = db.Column(db.DateTime, default = datetime.utcnow)
+
+   def __repr__(self) -> str:
+      return f"Task {self.id}"
+
+
+with app.app_context():
+      db.create_all()
+
+#Routing of the pages
+
+#Home route
+@app.route('/', methods = ['POST','GET'])
+def index():
+   #Add task
+   if request.method == 'POST':
+      current_task = request.form["contents"]
+      new_task = MyTask(content = current_task)
+      try:
+         db.session.add(new_task)
+         db.session.commit()
+         return redirect('/')
+      except Exception as e:
+         print(f"ERROR:{e} ")
+         return f"ERROR: {e}"
+   #Show Tasks available
+   else:
+      tasks = MyTask.query.order_by(MyTask.created).all()
+      return render_template('index.html', tasks = tasks)
+   
+
+
+#Delete route
+@app.route('/delete/<int:id>')
+def delete(id:int):
+   delete_task = MyTask.query.get_or_404(id)
+   try:
+      db.session.delete(delete_task)
+      db.session.commit()
+      return redirect('/')
+   except Exception as e:
+      return f"ERROR:{e}"
+
+#Edit route
+@app.route('/update/<int:id>', methods = ['GET','POST'])
+def edit(id:int):
+   edit_task = MyTask.query.get_or_404(id)
+   if request.method == "POST" :
+      edit_task.content = request.form["contents"]
+      try:
+         db.session.commit()
+         return redirect('/')
+      except Exception as e:
+         return f"ERROR:{e}"
+   else:
+      return render_template('edit.html', task = edit_task)
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
